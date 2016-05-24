41c368f3280ee5782f4ae80a6b0a6c55d3928c33
Robert Honer <robert@ribbon.co>
2016-05-23 18:29:57 -0700

    [#1] Adding support for bank accounts.

--- lib/payout/v1.rb
+++ lib/payout/v1.rb
@@ -1,5 +1,6 @@
 module Payout::V1
-  autoload(:Balance, 'payout/v1/balance')
-  autoload(:Card,    'payout/v1/card')
-  autoload(:Payment, 'payout/v1/payment')
+  autoload(:Balance,     'payout/v1/balance')
+  autoload(:Card,        'payout/v1/card')
+  autoload(:BankAccount, 'payout/v1/bank_account')
+  autoload(:Payment,     'payout/v1/payment')
 end
